69bceb1e1b52d64b89d54074e361a0c44c5ac97e
fpnav <fpnav@yahoo.com.br>
2016-10-07 08:44:27 -0300

    first commit

---
+++ ConsoleApplication1/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Contexto : DbContext
+    {
+        public Contexto()
+            : base("defaultConnection")
+        {
+
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PodeInserirUmAlunoNaBase()
+        {
+            Aluno aluno = new Aluno();
+            aluno.Id = 1;
+            aluno.Nome = "John";
+            aluno.Email = "john@teste.com";
+            Contexto db = new Contexto();
+            db.Alunos.Add(aluno);
+            db.SaveChanges();
+
+            var alunoFromDb = db.Alunos.Find(1);
+            Assert.IsNotNull(alunoFromDb);
+        }
+    }
+}
